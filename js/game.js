a489701896baec10c09d5930e32da291e676a2c7
soadaa <Soadhlh@gmail.com>
2022-06-02 17:16:56 -0400

    Add tied function

--- js/game.js
+++ js/game.js
@@ -3,8 +3,9 @@
 function randomNum() {
     return Math.floor(Math.random()*100+1)
 }
-let cpuPick;
 
+let cpuPick;
+// My work around to get the cpu to pick a number 1, 2 or 3.
 function cpuRandom() {
     cpuPick = randomNum();
     if (cpuPick == 100) {
@@ -21,19 +22,32 @@ function cpuRandom() {
 }
 
 let userPick;
-
 function getUserPick() {
     userPick = prompt("Pick 1 for Rock, 2 for Paper, and 3 for Scissor", "");
     userPick = parseInt(userPick);
 }
 
+let userScore = 0;
+let cpuScore = 0;
+let score = `You are at ${userScore} points while cpu is at ${cpuScore}`
+
+// First run of functions to get symbols from each player
+cpuRandom();
+getUserPick();
+getOutcome();
+
+// if function to see which symbol cpu picked, each with their
+// own switch function to make decisions based on userPick.
 function getOutcome () {
     if (cpuPick === 1) {
+        console.log("Cpu picked Rock");
         switch (userPick) {
             case 1:
                 console.log("You tied, try again");
+                console.log(score);
                 cpuRandom();
                 getUserPick();
+                getOutcome();
                 break;
             case 2:
                 console.log("You won this round");
@@ -47,6 +61,7 @@ function getOutcome () {
                 break;
         }
     } else if (cpuPick === 2) {
+        console.log("Cpu picked Paper");
         switch (userPick) {
             case 1:
                 console.log("The computer wins this round :(");
@@ -55,8 +70,10 @@ function getOutcome () {
                 break;
             case 2:
                 console.log("You tied, try again");
+                console.log(score);
                 cpuRandom();
                 getUserPick();
+                getOutcome();
                 break;
             case 3:
                 console.log("You won this round");
@@ -65,6 +82,7 @@ function getOutcome () {
                 break;
         }
     } else if (cpuPick === 3) {
+        console.log("Cpu picked Scissor");
         switch (userPick) {
             case 1:
                 console.log("You won this round");
@@ -78,8 +96,10 @@ function getOutcome () {
                 break;
             case 3:
                 console.log("You tied, try again");
+                console.log(score);
                 cpuRandom();
                 getUserPick();
+                getOutcome();
                 break;
         }
     } else {
@@ -87,5 +107,11 @@ function getOutcome () {
     }
 }
 
-cpuRandom();
-getUserPick();
+//Functions to keep score
+function win() {
+
+}
+
+function lose() {
+
+}
