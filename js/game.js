134cd0e4b5cd464287c2aab3449ebfef4f47687f
soadaa <Soadhlh@gmail.com>
2022-06-02 16:28:52 -0400

    Add function to ask user for their choice

--- js/game.js
+++ js/game.js
@@ -3,17 +3,26 @@
 function randomNum() {
     return Math.floor(Math.random()*100+1)
 }
+let cpuPick;
 
-let cpuPick = randomNum();
-if (cpuPick == 100) {
-    randomNum();
-} else if (cpuPick >= 67) {
-    cpuPick = 3;
-} else if (cpuPick >= 34) {
-    cpuPick = 2;
-} else if (cpuPick >= 1) {
-    cpuPick = 1;
-} else {
-    console.log("bug")
+function cpuRandom() {
+    cpuPick = randomNum();
+    if (cpuPick == 100) {
+        randomNum();
+    } else if (cpuPick >= 67) {
+        cpuPick = 3;
+    } else if (cpuPick >= 34) {
+        cpuPick = 2;
+    } else if (cpuPick >= 1) {
+        cpuPick = 1;
+    } else {
+        console.log("bug")
+    }
 }
 
+let userPick;
+
+function getUserPick() {
+    userPick = prompt("Pick 1 for Rock, 2 for Paper, and 3 for Scissor", "");
+    userPick = parseInt(userPick);
+}
