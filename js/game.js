b0ec07ba990eea82dd7a0e5a17f5ac9530e8c82f
soadaa <Soadhlh@gmail.com>
2022-06-02 16:11:52 -0400

    Change cpuOutcome to cpuPick and take out unneeded step

--- js/game.js
+++ js/game.js
@@ -4,17 +4,16 @@ function randomNum() {
     return Math.floor(Math.random()*100+1)
 }
 
-let cpuOutcome;
-
 let cpuPick = randomNum();
 if (cpuPick == 100) {
     randomNum();
 } else if (cpuPick >= 67) {
-    cpuOutcome = 3;
+    cpuPick = 3;
 } else if (cpuPick >= 34) {
-    cpuOutcome = 2;
+    cpuPick = 2;
 } else if (cpuPick >= 1) {
-    cpuOutcome = 1;
+    cpuPick = 1;
 } else {
     console.log("bug")
 }
+
